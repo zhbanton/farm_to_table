fdfb1b8b096083a5180f1cad7a01390555ef402c
zhbanton <zhbanton@gmail.com>
2014-08-24 05:35:29 -0400

    Posting model returns array of available pickup days based on Farm business days

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -19,6 +19,10 @@ class Posting < ActiveRecord::Base
 
   validates :quantity, :unit, :price_per_unit, :starting_date, :expiration_date, presence: true
 
+  def available_pickup_days
+    (self.starting_date..self.expiration_date).find_all { |day| product.farm.business_days.map(&:day).include? day.strftime("%A") }
+  end
+
   def total_value
     price_per_unit * quantity
   end
