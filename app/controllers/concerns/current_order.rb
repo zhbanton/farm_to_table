403b5f270434a140e494722778d1c79db391439e
zhbanton <zhbanton@gmail.com>
2014-08-26 13:49:03 -0400

    New orders are initialized with organization id and is completed set to false

--- app/controllers/concerns/current_order.rb
+++ app/controllers/concerns/current_order.rb
@@ -7,7 +7,7 @@ module CurrentOrder
   def set_order
     @order = Order.find(session[:order_id])
   rescue ActiveRecord::RecordNotFound
-    @order = Order.create
+    @order = Order.create(organization: current_user.role, is_completed: false)
     session[:order_id] = @order.id
   end
 
