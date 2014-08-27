c0a004e7dd9ab1fdb6c721727d148c3f7d77d3f4
zhbanton <zhbanton@gmail.com>
2014-08-27 15:12:05 -0400

    Order Item that exceeds posting quantity remaining cant be created

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -19,9 +19,16 @@ class OrderItem < ActiveRecord::Base
 
   validates :quantity, :pickup_date, :price_per_unit, :unit, presence: true
   validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+  validate :quantity_remaining_greater_than_zero
 
   def total_cost
     quantity * price_per_unit
+  end
+
+  def quantity_remaining_greater_than_zero
+    if posting.quantity_remaining - self.quantity < 0
+      errors.add(:quantity, "can't exceed amount farm has in stock")
+    end
   end
 
 end
